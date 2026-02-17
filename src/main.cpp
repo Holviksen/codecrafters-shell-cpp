ede509b750aba8f91f6bf55e7e962bc06b8a2e40
Holviksen <marcus.holvik@gmail.com>
2026-02-17 20:48:10 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  std::cout << std::unitbuf << std::flush;
+  std::cerr << std::unitbuf << std::flush;
 
   // TODO: Uncomment the code below to pass the first stage
   
