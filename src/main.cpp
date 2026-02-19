43f4d9be4c1a882b2b79a12f21149767debee728
Holviksen <marcus.holvik@gmail.com>
2026-02-19 14:04:40 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -54,13 +54,13 @@ void REPL(std::vector<std::string> &command_buffer){
 	  	std::getline(std::cin, input);
 		tokenize_input(input, command_buffer);
 	  	if(command_buffer[0] == "exit") break;
-		if(command_buffer[0] == "echo"){
+		else if(command_buffer[0] == "echo"){
 			for(int i = 1; i < command_buffer.size(); i++){
 				std::cout << command_buffer[i]<<" ";
 			}
 			std::cout<<std::endl;
 		}
-		if(command_buffer[0] == "type"){
+		else if(command_buffer[0] == "type"){
 			if (is_builtin(command_buffer[1])) {
 			  	std::cout << command_buffer[1] << " is a shell builtin\n";
 		  	}
@@ -83,30 +83,25 @@ bool is_builtin(const std::string& s) {
 bool is_exec(const std::string& s){
     for(const auto& dir : PATH){
         fs::path candidate = dir / s;
-
         if(fs::exists(candidate) && fs::is_regular_file(candidate)){
-#if defined(_WIN32)
-            // On Windows, just check extension
-            std::string ext = candidate.extension().string();
-            if(ext == ".exe" || ext == ".bat" || ext == ".cmd") {
-                std::cout << s << " is " << candidate.string() << std::endl;
-                return true;
-            }
-#elif defined(__unix__) || defined(__APPLE__)
-            // On POSIX, check execute permissions
-            fs::perms p = fs::status(candidate).permissions();
-            if((p & fs::perms::owner_exec) != fs::perms::none ||
-               (p & fs::perms::group_exec) != fs::perms::none ||
-               (p & fs::perms::others_exec) != fs::perms::none) 
-            {
-                std::cout << s << " is " << candidate.string() << std::endl;
-                return true;
-            }
-#else
-            // fallback: just accept regular files
-            std::cout << s << " is " << candidate.string() << std::endl;
-            return true;
-#endif
+			#if defined(_WIN32)
+            	std::string ext = candidate.extension().string();
+           	 	if(ext == ".exe" || ext == ".bat" || ext == ".cmd") {
+                	std::cout << s << " is " << candidate.string() << std::endl;
+                	return true;
+            	}
+			#elif defined(__unix__) || defined(__APPLE__)
+            	fs::perms p = fs::status(candidate).permissions();
+            	if((p & fs::perms::owner_exec) != fs::perms::none ||
+               	   (p & fs::perms::group_exec) != fs::perms::none ||
+                   (p & fs::perms::others_exec) != fs::perms::none){
+					std::cout << s << " is " << candidate.string() << std::endl;
+                	return true;
+				   }
+			#else
+            	std::cout << s << " is " << candidate.string() << std::endl;
+            	return true;
+			#endif
         }
     }
     return false;
