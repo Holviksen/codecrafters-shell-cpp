c7833990ae11f53f7437d5c8dd0885a75ed5c2c6
Holviksen <marcus.holvik@gmail.com>
2026-02-19 13:52:34 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -43,30 +43,7 @@ int main() {
   std::cerr << std::unitbuf << std::flush;
 
   
-  while(true){
-	  std::cout << "$ " << std::flush;
-	  std::string input;
-	  std::getline(std::cin, input);
-	  if (input == "exit") break;
-	  if (input.substr(0, 4) == "echo") {
-		  input.erase(0, 5);
-		  std::cout << input<<'\n';
-	  }
-	  else if (input.substr(0, 4) == "type") {
-		  input.erase(0, 5);
-		  if (is_builtin(input)) {
-			  std::cout << input << " is a shell builtin\n";
-		  }
-		  else if(){}
-		  else {
-			  std::cout << input << ": not found\n";
-		  }
-	  }
-	  else {
-		  std::cout << input << ": command not found\n";
-	  }
-	  
-  }
+  REPL(command_buffer);
   
 }
 
@@ -76,7 +53,13 @@ void REPL(std::vector<std::string> &command_buffer){
 	  	std::string input;
 	  	std::getline(std::cin, input);
 		tokenize_input(input, command_buffer);
-	  	
+	  	if(command_buffer[0] == "exit") break;
+		if(command_buffer[0] == "echo"){
+			for(int i = 1; i < command_buffer.size(); i++){
+				std::cout << command_buffer[i]<<" ";
+			}
+			std::cout<<std::endl;
+		}
 		if(command_buffer[0] == "type"){
 			if (is_builtin(command_buffer[1])) {
 			  	std::cout << command_buffer[1] << " is a shell builtin\n";
@@ -84,6 +67,9 @@ void REPL(std::vector<std::string> &command_buffer){
 			else if(!is_exec(input)){
 				std::cout << input << ": not found\n";
 			}
+		}
+		else{
+			std::cout << command_buffer[0] << ": command not found\n";
 		}
 		
 		command_buffer.clear();
